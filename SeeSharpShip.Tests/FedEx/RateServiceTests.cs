3537d6f83eb09aeb236bc0de0bd0805b653fda76
Sumo <msumerano@gmail.com>
2012-09-02 16:46:32 -0400

    Began working on FedEx implementation

---
+++ SeeSharpShip.Tests/FedEx/RateServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SeeSharpShip.Tests.FedEx
+{
+    [TestFixture]
+    public class RateServiceTests
+    {
+        [Test]
+        public void Get_EmptyRequest_ThrowsException() { 
+            var service = new Services.FedEx.RateService();
+            RateRequest request = new RateRequest();
+            Assert.Throws(typeof (Exception), () => service.Get(request));
+        }
+    }
+}
---
+++ SeeSharpShip/Services/FedEx/RateService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SeeSharpShip.FedExRateService;
+
+namespace SeeSharpShip.Services.FedEx
+{
+    public class RateService
+    {
+        //private RateReply CallRateService(RateRequest request) { return new RateReply(); }
+        public RateResponse Get(RateRequest request) {
+            var service = new FedExRateService.RateService();
+            var response = service.getRates(request);
+
+
+        }
+    }
+}
