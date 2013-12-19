710daedd8b2f21b4a71efc62a8a4f8356fa4c241
Sumo <msumerano@gmail.com>
2013-12-19 02:15:16 -0500

    Moved service proxies to the ServiceProxy project

---
+++ SeeSharpShip.ServiceProxy/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿#region SeeSharpShip.ServiceProxy is Copyright (C) 2011-2013 Michael J. Sumerano.
+
+// This file is part of SeeSharpShip.ServiceProxy.
+// 
+// SeeSharpShip.ServiceProxy is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// SeeSharpShip.ServiceProxy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with SeeSharpShip.ServiceProxy.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("SeeSharpShip.ServiceProxy")]
+[assembly: AssemblyDescription("Contains all necessary service proxies for external service-based APIs that SeeSharpShip relies on")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("6e18c712-72d3-4e61-990a-70aacf1dc5b4")]
